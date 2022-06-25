17270a66c2781ba5f24d3db2fb1349da113f7c1f
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2022-06-25 14:13:40 +0900

    feat: api 메인 클래스 구현 (#6)

--- libs/toss-payments.ts
+++ libs/toss-payments.ts
@@ -1,1 +1,28 @@
-export class TossPaymentsAPI {}
+import axios, { AxiosInstance } from 'axios';
+
+export const BASE_URL = 'https://api.tosspayments.com/v1';
+export interface TossPaymentsConfig {
+  /**
+   * 시크릿 키는 토스페이먼츠 API를 호출할 때 사용되는 키입니다. 노출되면 안됩니다.
+   */
+  secretKey: string;
+  /**
+   * 토스페이먼츠 API를 호출할 https endpoint 입니다.
+   */
+  baseURL: string;
+}
+
+export class TossPaymentsAPI {
+  httpClient: AxiosInstance;
+  constructor(config: TossPaymentsConfig) {
+    const { secretKey, baseURL } = config;
+    const base64key = Buffer.from(secretKey + ':', 'utf-8').toString('base64');
+
+    this.httpClient = axios.create({
+      baseURL,
+      headers: {
+        Authorization: `Bearer ${base64key}`,
+      },
+    });
+  }
+}
