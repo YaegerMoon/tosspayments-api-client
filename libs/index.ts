b81af464046ddb3dac28d93baf3fd3886b9d2c03
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2022-07-11 15:51:03 +0900

    chore: npmignore 설정 (#27)
    * chore(yarn): yarn 으로 프로젝트 초기 설정
    
    * chore(typescript): typescript 환경 구성 및 기본 설정 (#2)
    
    * chore(lint): lint와 prettier 환경 설정 (#3)
    
    * chore(lint-staged): precommit 단계에서 lint를 자동적으로 실행 (#4)
    
    * chore(husky): 커밋 단계에서 자동적으로 commitlint 동작 (#5)
    
    * feat: api 메인 클래스 구현 (#6)
    
    * feat(payments): 결제 관련 API 모듈  구현 (#7)
    
    * feat(payments): 결제(payments) api 3개 추가 (#8)
    
    * feat(billing): 자동결제(billing) 객체를 interface로 정의 (#10)
    
    * feat(billing): 자동결제 관련 API 호출 메서드 (#12)
    
    * feat(promotion): 카드할인 관련 API 호출 메서드 구현 (#14)
    
    * feat(promotion): 카드할인 관련 API 호출 메서드 구현
    * feat: resource api 정의
    
    * feat(settlement): 정산 관련 API 모듈 구현 (#16)
    
    * feat(transaction): transaction 관련 API 모듈 (#18)
    
    * feat(cash-receipt): 현금 영수증 관련 API 모듈 (#20)
    
    * chore(test): jest 기본설정 (#21)
    
    * feat(payments): 메서드 이름 변경 (#22)
    
    * test: :memo: make unit test template (#23)
    
    * test(payments): 결제관련 테스트 케이스 추가 (#24)
    
    Co-authored-by: YaegerMoon <yaeger@mass-adoption.com>
    
    * chore(version): apply standard-version (#25)
    
    Co-authored-by: YaegerMoon <yaeger@mass-adoption.com>
    
    * chore(release): 1.1.0
    
    * chore: npmignore 설정
    
    * chore: npmignore 설정
    
    Co-authored-by: YaegerMoon <yaeger@mass-adoption.com>

--- libs/index.ts
+++ libs/index.ts
@@ -1,2 +1,31 @@
-export * from './enum';
-export * from './toss-payments';
+import {
+  ACCOUNT_TYPE,
+  ACQUIRE_STATUS,
+  PAYMENT_STATUS,
+  PayMethod,
+  PaymentType,
+  EasyPay,
+  CardCompanyCode,
+  CartType,
+  BankCode,
+  REFUND_STATUS,
+  MOBILE_OPERATOR,
+} from './enum';
+import { TOSS_PAYMENTS_API_BASE_URL, TossPaymentsConfig, TossPaymentsAPI } from './toss-payments';
+
+export {
+  ACCOUNT_TYPE,
+  ACQUIRE_STATUS,
+  PAYMENT_STATUS,
+  PayMethod,
+  PaymentType,
+  EasyPay,
+  CardCompanyCode,
+  CartType,
+  BankCode,
+  REFUND_STATUS,
+  MOBILE_OPERATOR,
+  TOSS_PAYMENTS_API_BASE_URL,
+  TossPaymentsConfig,
+  TossPaymentsAPI,
+};
